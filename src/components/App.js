5d7e18cac22fb374a2a06a1e716118e669dbf490
Emme Buentiempo <emmettbuentiempo@gmail.com>
2020-11-17 14:34:07 -0800

    create initial site.
    Co-authored-by: Jeff Dinsmore <hello@jeffdinsmore.com>
    Co-Authored-By: Michael Watts <wattsjmichael@gmail.com>

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Header";
+import DayControl from "./DayControl";
+
+function App(){
+  
+  return (
+    <React.Fragment>
+      <Header />
+      <DayControl />
+    </React.Fragment>
+  );
+}
+
+export default App;
